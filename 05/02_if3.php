21f7aa26d6839d31f19224a9f128f8af0f168cfd
TakayukiKomada <Takakoma.79@gmail.com>
2022-07-19 13:51:01 +0900

    制御構文2の課題

---
+++ 05/02_if3.php
@@ -0,0 +1,10 @@
+<?php
+
+$poket_money = 1000;
+$fund_raising = 101;
+
+while ($poket_money > 101) {
+    echo "あなたの所持金は" . $poket_money . "円です。" . PHP_EOL . $fund_raising . "円募金しました｡" . PHP_EOL;
+    $poket_money = $poket_money - $fund_raising;
+}
+echo "あなたの所持金は" . $poket_money . "です。" . PHP_EOL . "あなたはこれ以上募金できません｡";
