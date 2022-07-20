7d2f7b4754a3ff39ad216081a3c3415d0f873241
TakayukiKomada <Takakoma.79@gmail.com>
2022-07-20 19:57:07 +0900

    修正中

---
+++ 06/03_form.php
@@ -0,0 +1,47 @@
+<?php
+$age = '';
+$err_msg = '';
+$msg = "";
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+$age = $_POST['age'];
+
+
+if (empty($age)) {
+$err_msg = '・年齢を入力してください。';
+} else {
+$msg = "私は{$age}歳です";
+}
+}
+?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>POSTメソッド</title>
+</head>
+
+<body>
+    <h1>POSTメソッド</h1>
+    <form action="" method="POST">
+        <div>
+            <label for="">年齢</label><br>
+            <input type="" name="age" value="">
+            <?php if (!empty($err_msg)) : ?>
+                <ul>
+                    <li><?= $err_msg ?></li>
+                </ul>
+            <?php endif; ?>
+        </div>
+        <div>
+            <input type="submit" value="送信">
+        </div>
+    </form>
+    <div>
+        <?= htmlspecialchars($msg, ENT_QUOTES, 'UTF-8') ?>
+    </div>
+</body>
+
+</html>
