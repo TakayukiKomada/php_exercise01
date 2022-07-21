5943d6fb5c0325d503b81bd9e1a18ead3d1f528f
TakayukiKomada <Takakoma.79@gmail.com>
2022-07-21 21:35:53 +0900

    フォームの課題

--- 06/05_form.php
+++ 06/05_form.php
@@ -1,10 +1,4 @@
 <?php
-$count1 = '';
-$count2 = '';
-$count3 = '';
-$err_msg = '';
-$msg = '';
-$sum = '';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $count1 = $_POST['count1'];
@@ -14,7 +8,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (is_numeric($count1) && is_numeric($count2) && is_numeric($count3)) {
         $sum = $count1 + $count2 + $count3;
     } else {
-        $err_msg = '数字を入力してください。';
+        $err_msg = '全てに数字を入力してください。';
     }
 }
 ?>
@@ -23,27 +17,25 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 <head>
     <meta charset="UTF-8">
-    <title>全てに数字を入力してください</title>
+    <title>数字を入力してください</title>
 </head>
 
 <body>
-    <h1>全てに数字を入力してください</h1>
+    <h1>数字を入力してください</h1>
     <form action="" method="POST">
         <div>
-            <ul>
-                <li>全てに数字を入力してください</li>
-            </ul><br>
+            <?php if (isset($err_msg)) : ?>
+                <ul>
+                    <li><?= $err_msg ?></li>
+                </ul>
+            <?php endif; ?>
             <p>1つ目の数字</p>
             <input type="number" name="count1">
             <p>2つ目の数字</p>
             <input type="number" name="count2">
             <p>3つ目の数字</p>
             <input type="number" name="count3">
-            <?php if (isset($err_msg)) : ?>
-                <ul>
-                    <li><?= $err_msg ?></li>
-                </ul>
-            <?php endif; ?>
+
         </div>
         <div>
             <input type="submit" value="送信">
