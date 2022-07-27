5fde5b2fa23323bcb9e3d88877b1df4acbb9b591
TakayukiKomada <Takakoma.79@gmail.com>
2022-07-28 08:10:02 +0900

    関数の課題

---
+++ 08/02_main.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once __DIR__ . '/02_functions.php';
+
+$yesterday_meal = [
+    '食パン' => 300, 
+    'ステーキ' => 1200, 
+    'うどん' => 500
+];
+
+$today_meal = [
+    'シリアル' => 200, 
+    'とんかつ' => 1000,
+    'パスタ' => 400
+];
+$total_meals = array_merge($today_meal, $yesterday_meal); 
+
+echo calc_cal($total_meals);
